e61be9ae023cbad5097bfdf577b8ee58911e8c85
tipsharsha <tipsharsha@gmail.com>
2023-02-12 20:19:48 +0530

    Maybe my last commit

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,6 +1,5 @@
 //Assignment 1//
 #include "macros.h"
-#include "px.c"
 //Group 27 //
 //2021AAPS0717H//
 
@@ -28,16 +27,96 @@
 // Task 8. Finally, the controller process will calculate the average of n values of wpapx as fapx and
 // report/print to the console the value of fapx.
 
+//Global Variables
 int n;
 int a;
 int b;
 int p;
-int** pi;//Global Variables
+int** pi;
 char wrt[25] = "This is a message";
 char rd[25]="Maybe";
 int* wpax;
 int fapx;
+int **fd;
 
+//Generation of px for x and returning thpax
+int prchk(int num)
+{
+    for(int i=2; i<num; i++)
+    {
+        if(num%i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int arr_prime(int p,int k)
+{
+    int* px =(int*) malloc(p*sizeof(int*)); 
+    int num=0;
+    int t = 0;
+    int sum;
+    // printf("Enter a Number");
+    // scanf("%d", &k);
+    for(int i=k+1; ; i++)
+    {
+        if(prchk(i) == 1)
+        {
+            num++;
+            px[num-1] = i;
+            if(num==p)
+            break;
+        }
+    }
+    // for(int z=0; z<p; z++)
+    // {
+    //     printf("%d ", px[z]);
+    // }
+    int m=0;
+    int*qx = (int*)malloc(p*sizeof(int*));
+    for(int i =0;i<p;i++)
+    {
+        qx[i]=1;
+    }
+    for(int j=k-1; j>1; j--)
+    {
+        if(prchk(j) == 1)
+        {
+            m++;
+            qx[p-m] = j;
+            t = p-m;
+            if(m==p || j==2)
+            break;
+        }
+    }
+    // for(int w=0; w<p; w++)
+    // {
+    //     printf("%d ", qx[w]);
+    // }
+    printf("%d \n", t);
+    
+    int y = prchk(k);
+    
+    int fx[2*p+y-t];
+    
+    for(int s=0; s<=p-t; s++)
+    {
+        fx[s] = qx[s+t];
+    }
+    if(y==1)
+    fx[p-t]=k;
+    for(int i=p-t+y; i<2*p+y-t; i++)
+    {
+        fx[i]=px[i-p+t-y];
+    }
+    for(int w=0; w<2*p+y-t; w++)
+    {
+        sum += px[w];
+    }
+    return sum/k;
+}
+//Row validator
 int validate_row(int* p,int min,int max,int count)//Function each worker uses to validate each row
 {
     for (int i =0;i<count;i++)
@@ -46,15 +125,40 @@ int validate_row(int* p,int min,int max,int count)//Function each worker uses to
     }
     return 0;
 }
-
-int* px_for_x(int x)
+//Thread function
+void *thr (void *param)
 {
-    int* p;
-
-    return p;
+    int x = *((int*)param);
+    arr_prime(x,n);
+    pthread_exit(0);
 }
-
-
+//WORKER FUNCTION
+void do_worker(int i)
+{
+    close(fd[i][1]);
+    read(fd[i][0],rd,25);
+    close(fd[i][1]);
+    if(validate_row(pi[i],a,b,n)){printf("Entered arguments aren't valid put them in range");exit(0);}
+    printf("%s \n",rd);
+    for(int j=0;j<n;j++)
+    {
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_create(&tid,&attr,thr,(pi[i]+j));
+    }
+}
+//CONTROLLER FUNCTION
+void do_control(int i)
+{
+    sleep(100);
+    close(fd[i][0]);
+    write(fd[i][1],wrt,strlen(wrt)+1);
+    close(fd[i][1]);
+    printf("I am a parent\n");
+    printf("Child Complete\n");
+}
+// MAIN FUNCTION
 int main(int argc,char *argv[])
 {
     n = atoi(argv[1]);
@@ -64,8 +168,11 @@ int main(int argc,char *argv[])
     p = atoi(argv[4]);
     wpax = (int*)malloc(n*sizeof(int));
     pi = (int**)malloc(n*sizeof(int*)) ;
+    fd = (int**)malloc(n*sizeof(int*)) ;
     for (int i =0;i<n;i++)
     pi[i] = (int*)malloc(n*sizeof(int));
+    for (int i =0;i<n;i++)
+    fd[i] = (int*)malloc(2*sizeof(int));
     for (int i =0;i<n;i++)
     {
         for (int j =0;j<n;j++)
@@ -74,7 +181,7 @@ int main(int argc,char *argv[])
         }
     }
     //Creating the pipe for all the workers
-    int fd[n][2];
+    
     for(int i=0;i<n;i++)
     { if (pipe(fd[i])==-1)
     {
@@ -93,31 +200,20 @@ int main(int argc,char *argv[])
         else if (pid[i]==0)
         {
             //Child pocesses
-            close(fd[i][1]);
-            read(fd[i][0],rd,25);
-            close(fd[i][1]);
-            if(validate_row(pi[i],a,b,n)){printf("Entered arguments aren't valid put them in range");exit(0);}
-            printf("%s \n",rd);
-            // execlp("ls","ls","-lh",NULL);
+            do_worker(i);
             return -1;
         }
-        else
-        {
-            //Parent process
-            sleep(100);
-            close(fd[i][0]);
-            write(fd[i][1],wrt,strlen(wrt)+1);
-            close(fd[i][1]);
+     
+            //Control process
+            do_control(i);
+     }
             int l;
-            printf("I am a parent\n");
-            printf("Child Complete\n");
             for(int i;i<n;i++)
             {
                 l += wpax[i];
             }
             fapx = l/n;
-        }
-     }
+     
 
 
     // for (int i =0;i<n;i++)//for checking the matrix
---
+++ simplesignal.c
@@ -0,0 +1,47 @@
+#define Project
+
+
+
+#ifdef Project
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PRINT_INFO(MSG, ...) { \
+	printf ( "%s INFO %d:%d %ld %s %s %d : " MSG ";;\n", \
+	"TODO_PRINT_TIME", getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+	__LINE__,  ##__VA_ARGS__); \
+}
+
+#define PRINT_ERROR(MSG, ...) { \
+	printf ( "%s ERROR %d:%d %ld %s %s %d : [%d] " MSG ";;\n", \
+	"TODO_PRINT_TIME", getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+	__LINE__,  errno, ##__VA_ARGS__);	\
+	}
+	
+#define PRINT_ERR_EXIT(MSG, ...) { \
+	printf ( "%s ERROR %d:%d %ld %s %s %d : [%d] " MSG ";;\n", \
+	"TODO_PRINT_TIME", getpid(), getppid(), pthread_self(), __FILE__, __FUNCTION__, \
+	__LINE__,  errno, ##__VA_ARGS__);	\
+	_exit(-1); \
+	}
+	
+#endif
+
+int main(argc,char **argv)
+{
+    
+}
+
+
+
+
+
+
